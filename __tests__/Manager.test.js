86b1130324717780db97fa5a0645460c9734a83f
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-06 15:48:03 -0500

    all tests and js set up but many failing tests

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,23 @@
+const Manager = require('../lib/Manager.js');
+
+test('new manager object created', () => {
+    const manager = new Manager('name');
+
+    expect(manager.name).toBe('name');
+    expect(manager.officeNumber).toBe('officeNumber');
+    expect(manager.role).toBe('manager');
+});
+
+// test for getOfficeNumber() method
+test("gets office number", () => {
+    const manager = new Manager('officeNumber');
+
+    expect(manager.getOfficeNumber().toBeDefined);
+});
+
+// test for getRole method 
+test ("gets employee role of Manager", () => {
+    const manager = new Manager('manager');
+
+    expect(manager.getRole().toBe('manager'));
+});
