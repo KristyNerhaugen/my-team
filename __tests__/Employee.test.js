5c2ddc12d8b07ca7902b3441ad256a319ad215d3
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-06 15:04:31 -0500

    updating employee test and js to include methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,4 +4,27 @@ test('new employee object created', () => {
     const employee = new Employee('name');
 
     expect(employee.name).toBe('name');
+    expect(employee.id).toBe('id');
+    expect(employee.email).toBe('email');
+});
+
+// test for getName method
+test("gets employee name", () => {
+    const employee = new Employee('name');
+    
+    expect(employee.getId().toBeDefined);
+});
+
+// test for getId method
+test("gets employee id", () => {
+    const employee = new Employee('id');
+
+    expect(employee.getName().toBeDefined);
+});
+
+// test for getEmail method
+test("gets employee email", () => {
+    const employee = new Employee('email');
+
+    expect(employee.getEmail().toBeDefined);
 });
