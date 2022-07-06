936438d59090f004a62aac932af45896a3db0ec6
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-06 15:17:15 -0500

    failing Engineer test with methods

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -4,4 +4,12 @@ test('new engineer object created', () => {
     const engineer = new Engineer('name');
 
     expect(engineer.name).toBe('name');
+    expect(engineer.gitHub).toBe('gitHub');
+});
+
+// test for getGithub method
+test("gets GitHub account information", () => {
+    const engineer = new Engineer('gitHub');
+
+    expect(engineer.getGitHub().toBeDefined);
 });
