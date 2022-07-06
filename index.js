25ebcf12c1b963326c40e339b826c7ad455b16ea
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-06 16:49:44 -0500

    finished questions and started function to write HTML

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Packages required for this application to run 
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const questions = () => {
     return inquirer.prompt([
         {
-            // user name
+            // employee name
             type: 'input',
             name: 'name',
             message: 'What is your name? (Required)',
@@ -14,6 +15,20 @@ const questions = () => {
                     return true;
                 } else {
                     console.log('Please enter your name.');
+                    return false;
+                }
+            }
+        },
+        {
+            // employee id number
+            type: 'input',
+            name: 'id',
+            message: 'What is your employee id number? (Required)',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your id number.');
                     return false;
                 }
             }
@@ -32,11 +47,71 @@ const questions = () => {
                 }
             }
         },
-    ]);
+        {
+            // employee role
+            type: 'checkbox',
+            name: 'role',
+            message: 'What is your employee role? Please check one role. (Required)',
+            choices: ['Manager', 'Engineer', 'Employee', 'Intern']
+        },
+        {
+            // confirm Manager for office number
+            type: 'confirm',
+            name: 'confirmManager',
+            message: 'Are you a Manager?',
+            default: false
+        },
+        {
+            // if Manager is confirmed, ask for office number
+            type: 'input',
+            name: 'officeNumber',
+            message: "Please enter your office number.",
+            when: ({ confirmManager }) => confirmManager
+        },
+        {
+            // confirm Engineer for gitHub account
+            type: 'confirm',
+            name: 'confirmEngineer',
+            message: 'Are you an Engineer?',
+            default: false
+        },
+        {
+            // if Engineer is confirmed, ask for GitHub username
+            type: 'input',
+            name: 'gitHub',
+            message: "Please enter your GitHub account username.",
+            when: ({ confirmEngineer }) => confirmEngineer
+        },
+        {
+            // confirm Intern for school information
+            type: 'confirm',
+            name: 'confirmIntern',
+            message: 'Are you an Intern?',
+            default: false
+        },
+        {
+            // if Intern is confirmed, ask for school information
+            type: 'input',
+            name: 'school',
+            message: "What school do you attend?",
+            when: ({ confirmIntern }) => confirmIntern
+        },
+    ])
+
+    // function to write HTML
+    .then(data => {
+        const filename = `./dist/${data.name}.html`;
+        fs.writeFile(filename, generateSite(data), err =>
+        err ? console.log(err) : console.log('Success! HTML page created!')
+        );
+    });
 };
 
+
+// function to initialize app
 function init() {
     questions();
 }
 
+// function to call initialize app
 init();
