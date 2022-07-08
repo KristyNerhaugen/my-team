3b7c9d5d659d48fba0223a3f393abf467a26765c
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-08 13:07:16 -0500

    css stylesheet linking to generated html

--- src/generate-site.js
+++ src/generate-site.js
@@ -3,11 +3,24 @@ const fs = require('fs');
 // Function to generate the HTML site
 function generateSite(data) {
     return `
-    <section> 
-        <h1> Employee name: ${data.name}</h1>
-        <h2> Employee Id Number: ${data.id}</h2>
-        <h2> Employee Email: <a href="mailto:${data.email}">${data.email}</a>
-    </section>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>My Team</title>
+        <link rel="stylesheet" href="/Users/kristy/Desktop/BootCamp/my-team/src/style.css">
+    </head>
+    <body>
+        <h1>My Team</h1>
+        <section class="card"> 
+            <h2> Employee name: ${data.name}</h2>
+            <h3> Employee Id Number: ${data.id}</h3>
+            <h3> Employee Email: <a href="mailto:${data.email}">${data.email}</a> <h3>
+        </section>
+    </body>
     `;
 }
 
