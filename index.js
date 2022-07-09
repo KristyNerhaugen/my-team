c671036368eb7ba6626a84a70b981821fd646c15
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-08 19:53:37 -0500

    trying to only render generate employee when employees are added

--- index.js
+++ index.js
@@ -12,208 +12,213 @@ const manager = require('./lib/Manager.js');
 
 // Function to prompt array of questions to build new team
 const newTeam = () => {
-    console.log('Hello Manager! Please provide some information to build your employee team profile.');
-    return inquirer.prompt([
-        { // ask for team name to generate the html file name
-            type: 'input',
-            name: 'teamName',
-            message: 'What is the name of your team? (Required)',
-            validate: teamNameInput => {
-                if (teamNameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a team name.');
-                    return false;
+   // function managerInfo() {
+        console.log('Hello Manager! Please provide some information to build your employee team profile.');
+        return inquirer.prompt([
+            { // ask for team name to generate the html file name
+                type: 'input',
+                name: 'teamName',
+                message: 'What is the name of your team? (Required)',
+                validate: teamNameInput => {
+                    if (teamNameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a team name.');
+                        return false;
+                    }
                 }
-            }
-        },
-        { // employee name for Manager
-            type: 'input',
-            name: 'managerName',
-            message: 'What is your name? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your name.');
-                    return false;
+            },
+            { // employee name for Manager
+                type: 'input',
+                name: 'managerName',
+                message: 'What is your name? (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your name.');
+                        return false;
+                    }
                 }
-            }
-        },
-        { // employee id number for Manager
-            type: 'number',
-            name: 'managerId',
-            message: 'What is your employee ID number? (Required and only number are accepted.)',
-            validate: idInput => {
-                if (idInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your ID number. Only number values are accepted.');
-                    return false;
+            },
+            { // employee id number for Manager
+                type: 'number',
+                name: 'managerId',
+                message: 'What is your employee ID number? (Required and only number are accepted.)',
+                validate: idInput => {
+                    if (idInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your ID number. Only number values are accepted.');
+                        return false;
+                    }
                 }
-            }
-        },
-        { // email address for Manager
-            type: 'input',
-            name: 'managerEmail',
-            message: 'What is your email address? (Required)',
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your email address.');
-                    return false;
+            },
+            { // email address for Manager
+                type: 'input',
+                name: 'managerEmail',
+                message: 'What is your email address? (Required)',
+                validate: emailInput => {
+                    if (emailInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your email address.');
+                        return false;
+                    }
                 }
-            }
-        },
-        { // ask Manager for office number
-            type: 'input',
-            name: 'officeNumber',
-            message: "What is your office number? (Required)",
-            validate: officeNumberInput => {
-                if (officeNumberInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your office number.');
-                    return false;
+            },
+            { // ask Manager for office number
+                type: 'input',
+                name: 'officeNumber',
+                message: "What is your office number? (Required)",
+                validate: officeNumberInput => {
+                    if (officeNumberInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your office number.');
+                        return false;
+                    }
                 }
-            }
-        },
-        { // employee role
-            type: 'list',
-            name: 'role',
-            message: 'What is the role of the employee you are adding? Please check one role. (Required)',
-            choices: ['Engineer', 'Intern'],
-            validate: roleInput => {
-                if (roleInput) {
-                    return true;
-                } else {
-                    console.log('Please select employee role.');
-                    return false;
+            },
+       // ])
+   // }
+   // function employeeInfo() {
+       // return inquirer.prompt([
+            { // employee role
+                type: 'list',
+                name: 'role',
+                message: 'What is the role of the employee you are adding? Please check one role. (Required)',
+                choices: ['Engineer', 'Intern'],
+                validate: roleInput => {
+                    if (roleInput) {
+                        return true;
+                    } else {
+                        console.log('Please select employee role.');
+                        return false;
+                    }
                 }
-            }
-        },
-        { // employee name for Engineer
-            type: 'input',
-            name: 'engineerName',
-            message: 'What is the name of the employee? (Required)',
-            when: (input) => input.role === "Engineer",
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter employee name.');
-                    return false;
+            },
+            { // employee name for Engineer
+                type: 'input',
+                name: 'engineerName',
+                message: 'What is the name of the employee? (Required)',
+                when: (input) => input.role === "Engineer",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter employee name.');
+                        return false;
+                    }
                 }
-            }
-        },
-        { // employee id number for Engineer
-            type: 'number',
-            name: 'engineerId',
-            message: 'What is the ID number for this employee? (Required and only number are accepted.)',
-            when: (input) => input.role === "Engineer",
-            validate: idInput => {
-                if (idInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the id number. Only number values are accepted.');
-                    return false;
+            },
+            { // employee id number for Engineer
+                type: 'number',
+                name: 'engineerId',
+                message: 'What is the ID number for this employee? (Required and only number are accepted.)',
+                when: (input) => input.role === "Engineer",
+                validate: idInput => {
+                    if (idInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the id number. Only number values are accepted.');
+                        return false;
+                    }
                 }
-            }
-        },
-        { // email address for Engineer
-            type: 'input',
-            name: 'engineerEmail',
-            message: 'What is the email address for this employee? (Required)',
-            when: (input) => input.role === "Engineer",
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the email address.');
-                    return false;
+            },
+            { // email address for Engineer
+                type: 'input',
+                name: 'engineerEmail',
+                message: 'What is the email address for this employee? (Required)',
+                when: (input) => input.role === "Engineer",
+                validate: emailInput => {
+                    if (emailInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the email address.');
+                        return false;
+                    }
                 }
-            }
-        },
-        {  // if Engineer is selected, ask for GitHub username
-            type: 'input',
-            name: 'gitHub',
-            message: "Please enter the GitHub account username for this Engineer. (Required)",
-            when: (input) => input.role === "Engineer",
-            validate: gitHubInput => {
-                if (gitHubInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the GitHub account username.');
-                    return false;
+            },
+            {  // if Engineer is selected, ask for GitHub username
+                type: 'input',
+                name: 'gitHub',
+                message: "Please enter the GitHub account username for this Engineer. (Required)",
+                when: (input) => input.role === "Engineer",
+                validate: gitHubInput => {
+                    if (gitHubInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the GitHub account username.');
+                        return false;
+                    }
                 }
-            }
-        },
-        { // employee name for Intern
-            type: 'input',
-            name: 'internName',
-            message: 'What is the name of the employee? (Required)',
-            when: (input) => input.role === "Intern",
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter employee name.');
-                    return false;
+            },
+            { // employee name for Intern
+                type: 'input',
+                name: 'internName',
+                message: 'What is the name of the employee? (Required)',
+                when: (input) => input.role === "Intern",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter employee name.');
+                        return false;
+                    }
                 }
-            }
-        },
-        { // employee id number for Intern
-            type: 'number',
-            name: 'internId',
-            message: 'What is the ID number for this employee? (Required and only number are accepted.)',
-            when: (input) => input.role === "Intern",
-            validate: idInput => {
-                if (idInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the ID number. Only number values are accepted.');
-                    return false;
+            },
+            { // employee id number for Intern
+                type: 'number',
+                name: 'internId',
+                message: 'What is the ID number for this employee? (Required and only number are accepted.)',
+                when: (input) => input.role === "Intern",
+                validate: idInput => {
+                    if (idInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the ID number. Only number values are accepted.');
+                        return false;
+                    }
                 }
-            }
-        },
-        { // email address for Engineer
-            type: 'input',
-            name: 'internEmail',
-            message: 'What is the email address for this employee? (Required)',
-            when: (input) => input.role === "Intern",
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the email address.');
-                    return false;
+            },
+            { // email address for Engineer
+                type: 'input',
+                name: 'internEmail',
+                message: 'What is the email address for this employee? (Required)',
+                when: (input) => input.role === "Intern",
+                validate: emailInput => {
+                    if (emailInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the email address.');
+                        return false;
+                    }
                 }
-            }
-        },
-        {  // if Intern is selected, ask for school information
-            type: 'input',
-            name: 'school',
-            message: "What school does the intern attend? (Required)",
-            when: (input) => input.role  === "Intern",
-            validate: schoolInput => {
-                if (schoolInput) {
-                    return true;
-                } else {
-                    console.log('Please enter school information.');
-                    return false;
+            },
+            {  // if Intern is selected, ask for school information
+                type: 'input',
+                name: 'school',
+                message: "What school does the intern attend? (Required)",
+                when: (input) => input.role  === "Intern",
+                validate: schoolInput => {
+                    if (schoolInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter school information.');
+                        return false;
+                    }
                 }
+            },
+            {
+                type: 'confirm',
+                name: 'anotherEmployee',
+                message: "Would you like to add another employee to your team list? Enter y for yes or n for no.",
+                default: false
             }
-        },
-        {
-            type: 'confirm',
-            name: 'anotherEmployee',
-            message: "Would you like to add another employee to your team list? Enter y for yes or n for no.",
-            default: false
-        }
-    ])
-
-    // function to add another employee if anotherEmployee prompt is confirmed "True" OR write HTML if confirmed "False"
+        ])
+    
+        // function to add another employee if anotherEmployee prompt is confirmed "True" OR write HTML if confirmed "False"
         .then(data => {
             if(data.anotherEmployee){
                 return newTeam();
@@ -225,7 +230,8 @@ const newTeam = () => {
                 err ? console.log(err) : console.log('Success! HTML page created!')
                 );
             }
-        });
+        })
+   // }        
 };
 
 // function to initialize app
@@ -234,4 +240,4 @@ function init() {
 }
 
 // function to call initialize app
-init();
+ init();
--- src/generate-site.js
+++ src/generate-site.js
@@ -1,5 +1,39 @@
 const fs = require('fs');
 
+// Function that returns engineer/intern information to HTML page only if engineer/intern information is passed in
+// If there is no new employee added, return an empty string
+function renderEmployeeCard(role) {
+    if (role === 'none'|| role === undefined) {
+      return '';
+    } else if (role === 'Engineer') {
+      return ` 
+        <div class="card" style="width: 18rem;">
+            <div class="card-header font-weight-bold">
+                Employee name: ${data.engineerName}
+                <br>Engineer
+            </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Employee Id Number: ${data.engineerId}</li>
+                    <li class="list-group-item">Employee Email: <a href="mailto:${data.engineerEmail}">${data.engineerEmail}</a></li>
+                    <li class="list-group-item">Engineer GitHub Account: <a href=https://github.com/${data.gitHub}>${data.gitHub}</a></li>
+                </ul>
+        </div>`
+    } else if (role === 'Intern') {
+      return `
+        <div class="card" style="width: 18rem;">
+            <div class="card-header font-weight-bold">
+                Employee name: ${data.internName}
+                <br>Intern
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Employee Id Number: ${data.internId}</li>
+                <li class="list-group-item">Employee Email: <a href="mailto:${data.internEmail}">${data.internEmail}</a></li>
+                <li class="list-group-item">Intern's school: ${data.school} </li>
+            </ul>
+        </div>`
+    }
+  }
+
 // Function to generate the HTML site
 function generateSite(data) {
     return `
@@ -26,27 +60,9 @@ function generateSite(data) {
                     <li class="list-group-item">Employee Id Number: ${data.managerId}</li>
                     <li class="list-group-item">Employee Email: <a href="mailto:${data.managerEmail}">${data.managerEmail}</a></li>
                     <li class="list-group-item">Manager Office Number: ${data.officeNumber}</li>
+                </ul>
             </div>
-            <div class="card" style="width: 18rem;">
-                <div class="card-header font-weight-bold">
-                    Employee name: ${data.engineerName}
-                    <br>Engineer
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Employee Id Number: ${data.engineerId}</li>
-                    <li class="list-group-item">Employee Email: <a href="mailto:${data.engineerEmail}">${data.engineerEmail}</a></li>
-                    <li class="list-group-item">Engineer GitHub Account: <a href=https://github.com/${data.gitHub}>${data.gitHub}</a></li>
-            </div>
-            <div class="card" style="width: 18rem;">
-            <div class="card-header font-weight-bold">
-                Employee name: ${data.internName}
-                <br>Intern
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Employee Id Number: ${data.internId}</li>
-                <li class="list-group-item">Employee Email: <a href="mailto:${data.internEmail}">${data.internEmail}</a></li>
-                <li class="list-group-item">Intern's school: ${data.school} </li>
-        </div>
+            ${renderEmployeeCard(data.role[0])}
         </section>
     </body>
     `;
